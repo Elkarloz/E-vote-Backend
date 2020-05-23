b3ec5862feef48880aa8f5d882df504e4f79f3c1
Elkarloz <croameneses@gmail.com>
2020-05-22 20:53:23 -0500

    Documentacion, Aspirante Actualizacion

--- src/routes/aspirante.js
+++ src/routes/aspirante.js
@@ -74,92 +74,15 @@ router.post('/Registrar', async function(req,res){
    [Codigo,Ruta,PDFRuta],(err,result)=>{
 
    try {
-      // doc pdf
-      doc.pipe(fs.createWriteStream(`./src/docs/Doc-${Apellido}-${Documento}.pdf`)); 
-      const titulo = 'FORMULARIO DE INSCRIPCION DE CANDIDATO A REPRESENTANTE DE APRENDICES 2020'
-      var jornada = req.body.Jornada 
-      var Nombre = (req.body.Nombre +(" ")+ req.body.Apellido)
-      var TipoDoc = req.body.Tipdoc
-      var NDoc = req.body.Documento
-      var NivelFor = req.body.Nivel
-      var ficha = req.body.Ficha
-      var Profor = req.body.Formacion
-      var correo = req.body.Correo1
-      var correo2 = req.body.Correo2                            
-      doc
-      .font('Times-Bold', 13)
-      .image('src/img/Sena.png', 400, 50, {width: 100})
-      .text(titulo, 100, 80,{
-         columns:2,
-         align: 'center'
-      });
+        
+      if (result[0][0].Mensaje=== 'No te puedes registrar, revisa si hoy es fecha de incricion a aspirante, o si existe un proceso de votacion activo') {
+         res.json(result);
+         console.log("No paso");
+     } else {
+         res.json(result);
+         console.log("Paso");
+     }
 
-      doc
-      .font('Times-Bold', 11)
-      .text('Regional Caquetá', 100,180)
-      .text('Centro tecnológico de la Amazonía', 350,180)
-
-      .text(`Jornada a la cual pertenece: _______________________`, 100,210)
-      .text(`${jornada}`, 270,210)
-
-      .text(`Nombre completo: _______________________________________________________________`, 100,240)
-      .text(`${Nombre}`, 200,240)
-      
-      .text(`Tipo de documento: ________________________`, 100,270)
-      .text(`${TipoDoc}`, 200,270)
-
-      .text(`N° documento: _____________________`, 350,270)
-      .text(`${NDoc}`, 450,270)
-
-      .text(`Nivel de formación: ________________________`, 100,300)
-      .text(`${NivelFor}`, 230,300)
-
-      .text(`N° de ficha: _______________________`, 350,300)
-      .text(`${ficha}`, 450,300)
-
-      .text(`Nombre del programa de formación: _______________________________________________`, 100,330)
-      .text(`${Profor}`, 300,330)
-
-      .text(`Email: _______________________________`, 100,360)
-      .text(`${correo}`, 150,360)
-
-      .text(`Email Misena: ________________________`, 330,360)
-      .text(`${correo2}`, 410,360);
-
-      doc
-      .font('Times-Bold', 11)
-      .text('COMO ASPIRANTE A REPRESENTANTE DE APRENDICES:', 100,400);
-
-      doc
-      .font('Times-Roman', 11)
-      .text('He leído y entendido el Acuerdo 00002 del 2014 y que cumplo las siguientes condiciones para como Representante de aprendices de mi jornada:', 100,420);
-
-      doc
-      .font('Times-Roman', 11)
-      .text('1.  Estoy vinculado a uno programa de formación técnica o tecnológica en el SENA', 120,450)
-      .text('2.  No he cursado más de tres (3) trimestres en mi programa de formación (9 meses)', 120,470)
-      .text('3.  No he sido representante de aprendices en ningún centro de formación del SENA', 120,490)
-      .text('4.  Tengo la aptitud de responsabilidad para asumir como Representante, sin descuidar las obligaciones de mi proceso de aprendizaje', 120,510)
-      .text('5.  No presento plan de mejoramiento, condicionamiento de matrícula, ni llamados de atención por escrito', 120,540)
-      .text('6. No presento antecedentes disciplinarios en la Policía', 120,570)
-
-      .font('Times-Roman', 11)
-      .text('Certifico con mi firma que la información aquí otorgada es real y me prometo asumir las responsabilidades como representante, en el caso de ser elegido, sin descuidar las obligaciones de mi proceso de formación.', 100,600)
-
-      .font('Times-Roman', 10)
-      .text('__________________________________________', 100,680)
-      .text('Firma del aspirante a candidato a Representante de aprendices', 100,700)
-
-      .font('Times-Roman', 10)
-      .text(`${f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear()}`, 430,680)
-      .text('______________________', 400,680)
-      .text('Fecha de inscripción día/mes/año', 400,700)
-
-
-      .image('src/img/Logo.png', 500,750, {width: 100})
-      doc.end(); //finaliza
-      console.log('archivo generado');   
-  
       
    } catch (error) {
       res.status(500).json({
